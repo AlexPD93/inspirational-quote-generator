1e2401b54e1ff5f0ddaca91ec853247fb150442e
Alex Perez-Davies <alexperezdavies@gmail.com>
2023-09-02 12:31:38 +0100

    Create front end design

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,29 @@
+import React, { useState } from "react";
 import Head from "next/head";
 
 import styles from "@/styles/Home.module.css";
-import { GradientBackgroundCon } from "../../components/QuoteGenerator/QuoteGeneratorElements";
+
+// Components
+import {
+  GradientBackgroundCon,
+  BackgroundImage1,
+  BackgroundImage2,
+  FootCon,
+  FooterLink,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorTitle,
+  QuoteGeneratorSubTitle,
+  GenerateQuoteButton,
+  GenerateQuoteButtonText,
+} from "../../components/QuoteGenerator/QuoteGeneratorElements";
+
+// Assets
+import Cloud1 from "../../assets/cloud-and-thunder.png";
+import Cloud2 from "../../assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0);
   return (
     <>
       <Head>
@@ -13,7 +33,65 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Background */}
-      <GradientBackgroundCon></GradientBackgroundCon>
+      <GradientBackgroundCon>
+        {/* Quote generator modal pop-up */}
+
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>
+              Daily Inspiration Generator
+            </QuoteGeneratorTitle>
+
+            <QuoteGeneratorSubTitle>
+              Looking for a splash of inspiration? Generate a quote card with a
+              random inspirational quote provided by{" "}
+              <FooterLink
+                href="https://zenquotes.io/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                ZenQuotes API
+              </FooterLink>
+              .
+            </QuoteGeneratorSubTitle>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText>Make a Quote</GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        <BackgroundImage1
+          src={Cloud1}
+          height="300"
+          alt="cloudybackground1"
+        ></BackgroundImage1>
+
+        <BackgroundImage2
+          src={Cloud2}
+          height="300"
+          alt="cloudybackground1"
+        ></BackgroundImage2>
+
+        <FootCon>
+          <>
+            Quotes Generated: {numberOfQuotes}
+            <br />
+            Developed with love by{" "}
+            <FooterLink
+              href="https://www.linkedin.com/in/aperezdavies/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Alex Perez-Davies
+            </FooterLink>
+          </>
+        </FootCon>
+      </GradientBackgroundCon>
     </>
   );
 }
